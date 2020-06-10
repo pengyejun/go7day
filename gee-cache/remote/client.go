f6594e4461eacf20576c76e127553e032dc2ec50
pengyejun <p15281826276@gmail.com>
2020-06-10 22:31:34 +0800

    add test

--- gee-cache/remote/client.go
+++ gee-cache/remote/client.go
@@ -1,17 +1,23 @@
 package remote
 
 import (
+	"bytes"
 	"fmt"
 	"gee-cache/geecache"
 	"gee-cache/geecachepb"
 	"github.com/golang/protobuf/proto"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 type httpGetter struct {
 	baseURL string
+}
+
+var bufferPool = sync.Pool{
+	New: func() interface{} { return new(bytes.Buffer) },
 }
 
 func (h *httpGetter) Get(in *geecachepb.Request, out *geecachepb.Response) error {
@@ -32,12 +38,14 @@ func (h *httpGetter) Get(in *geecachepb.Request, out *geecachepb.Response) error
 	if res.StatusCode != http.StatusOK {
 		return fmt.Errorf("remote returned: %v", res.Status)
 	}
-
-	resBytes, err := ioutil.ReadAll(res.Body)
+	resBytes := bufferPool.Get().(*bytes.Buffer)
+	resBytes.Reset()
+	defer bufferPool.Put(resBytes)
+	_, err = io.Copy(resBytes, res.Body)
 	if err != nil {
 		return fmt.Errorf("reading responce body: %v", err)
 	}
-	if err = proto.Unmarshal(resBytes, out); err != nil {
+	if err = proto.Unmarshal(resBytes.Bytes(), out); err != nil {
 		return fmt.Errorf("decoding response body: %v", err)
 	}
 	return nil
--- gee-cache/singleflight/singleflight.go
+++ gee-cache/singleflight/singleflight.go
@@ -10,6 +10,7 @@ type call struct {
 
 type Group struct {
 	m map[string]*call
+	mu sync.Mutex
 }
 
 func New() *Group {
@@ -17,19 +18,24 @@ func New() *Group {
 }
 
 func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
-
+	g.mu.Lock()
+	if g.m == nil {
+		g.m = make(map[string]*call)
+	}
 	if c, ok := g.m[key]; ok {
+		g.mu.Unlock()
 		c.wg.Wait()
 		return c.val, c.err
 	}
 	c := new(call)
 	c.wg.Add(1)
 	g.m[key] = c
-
+	g.mu.Unlock()
 	c.val, c.err = fn()
 	c.wg.Done()
-
+	g.mu.Lock()
 	delete(g.m, key)
+	g.mu.Unlock()
 	return c.val, c.err
 }
 
